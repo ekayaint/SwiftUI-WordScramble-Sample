e124aad2766fe604506ba85bbd63659229601678
ekayaint <ekayaint@gmail.com>
2023-09-09 14:50:28 +0300

    Initial Commit

---
+++ SwiftUI-WordScramble-Sample/SwiftUI_WordScramble_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WordScramble_SampleApp.swift
+//  SwiftUI-WordScramble-Sample
+//
+//  Created by ekayaint on 9.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WordScramble_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
